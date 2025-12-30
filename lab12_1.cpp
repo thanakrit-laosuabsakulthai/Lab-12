050913315939d788d47615d19929dd96c6301d9d
Thanakrit <thanakrit_la@cmu.ac.th>
2025-12-30 14:57:15 +0700

    lab12_1.cpp completed (version 1)

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+int fibonacci(int);
 
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int n)
+{
+    if (n <= 0) return 0;
+    else if (n == 1) return 1;
+    else return fibonacci(n - 1) + fibonacci(n - 2);
 }
