0016a7932974d2cdbcae9a00e5099a854d420cb6
Thanakrit <thanakrit_la@cmu.ac.th>
2025-12-30 15:10:37 +0700

    lab12_2.cpp completed

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int a, int b);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int a, int b){
+    if(a % b == 0) return b;
+    return gcd(b, a%b);
 }
